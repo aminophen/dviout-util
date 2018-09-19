5bda0553fa9e3d51cc0b920eca057cba85a01f03
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2018-09-19 23:26:29 +0900

    fix import/backport scripts

--- 00backport.sh
+++ 00backport.sh
@@ -9,7 +9,7 @@ done
 # with encoding conversion
 # GNU iconv can be used to convert UTF-8 -> CP932
 for x in 00texjporg/*.{c,h,txt}; do \
-	if [ "$$x" != "00texjporg/config.h" ]; then \
+	if [ "$x" != "00texjporg/config.h" ]; then \
 		iconv -f UTF-8 -t CP932 "$x" >"./${x##*/}"; \
 	fi \
 done
--- 00import.sh
+++ 00import.sh
@@ -9,7 +9,7 @@ done
 # with encoding conversion
 # GNU iconv can be used to convert CP932 -> UTF-8
 for x in *.{c,h,txt}; do \
-	if [ "$$x" != "config.h" ]; then \
+	if [ "$x" != "config.h" ]; then \
 		iconv -f CP932 -t UTF-8 "$x" >"00texjporg/$x"; \
 	fi \
 done
