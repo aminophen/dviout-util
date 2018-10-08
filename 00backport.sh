3c0718404cc2b1bd6041e72ed2403d983b175f3f
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2018-10-08 13:57:55 +0900

    import . -> 00texjporg (fix previous mistake)

--- 00backport.sh
+++ 00backport.sh
@@ -2,6 +2,7 @@
 # backport from TL tree (texjporg/tex-jp-build) -> standalone repo
 
 # without encoding conversion
+# (tests/jistest.pl is included but overwritten later)
 for x in 00texjporg/{README,COPYING,ChangeLog,*.man,*.test}; do \
 	cp "$x" "./${x##*/}"; \
 done
@@ -14,3 +15,4 @@ for x in 00texjporg/*.{c,h,txt}; do \
 		iconv -f UTF-8 -t CP932 "$x" >"./${x##*/}"; \
 	fi \
 done
+iconv -f UTF-8 -t CP932 "00texjporg/tests/jistest.pl" >"./tests/jistest.pl"
--- 00import.sh
+++ 00import.sh
@@ -2,13 +2,14 @@
 # import from standalone repo -> TL tree (texjporg/tex-jp-build)
 
 # without encoding conversion
+# (tests/jistest.pl is included but overwritten later)
 for x in {README,COPYING,ChangeLog,*.man,*.test,tests/*}; do \
 	cp "$x" "00texjporg/$x"; \
 done
 
 # with encoding conversion
 # GNU iconv can be used to convert CP932 -> UTF-8
-for x in *.{c,h,txt}; do \
+for x in *.{c,h,txt} tests/jistest.pl; do \
 	if [ "$x" != "config.h" ]; then \
 		iconv -f CP932 -t UTF-8 "$x" >"00texjporg/$x"; \
 	fi \
